44e272fdac44755f15cdb20497d72cf18296d73b
user <user@irvm.localdomain>
2025-02-26 16:32:05 +0100

    Memcheck update

--- Valgrind/Memcheck/memcheck.sh
+++ Valgrind/Memcheck/memcheck.sh
@@ -2,6 +2,6 @@
 
 set -xe
 
-valgrind --show-leak-kinds=all --leak-check=full --track-origins=yes --log-file="jungleSpeed.txt" ~/Desktop/jungle-speed/jungleSpeed/src/server/build/server
+valgrind --show-leak-kinds=all --leak-check=full --track-origins=yes --log-file="jungleSpeed3.txt" ~/Desktop/jungle-speed/jungleSpeed/src/server/build/server
 
 echo "memcheck finished"
